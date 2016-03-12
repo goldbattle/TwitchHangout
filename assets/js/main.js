f17b33def932e2528b67fe5caa1bdfd6eecf09a2
Patrick Geneva <patfgeneva@gmail.com>
2016-03-11 21:52:27 -0500

    dfs

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,19 +18,8 @@ gapi.hangout.data.onStateChanged.add(function(event) {
   var stream_obj = document.getElementById('live_player');
   // Get chat enbed
   var chat_embed = document.getElementById('chat_embed');
-  // Create our player
-  var options = {
-      width: '100%',
-      height: '100%',
-      channel: event.state["stream_input"], 
-      //video: "{VIDEO_ID}" 
-  };
-  try {
-    // Remove old
-    Twitch.Player.destroy()
-  } catch(v) {}
-  // Create new player
-  Twitch.Player = new Twitch.Player("live_player", options);
+  // Change channel
+  Twitch.Player.setChannel(event.state["stream_input"])
   // Update document
   stream_input.value = event.state["stream_input"];
   chat_embed.setAttribute("src", "http://www.twitch.tv/"+event.state["stream_input"]+"/chat");
@@ -39,10 +28,23 @@ gapi.hangout.data.onStateChanged.add(function(event) {
 // Called on chat open button
 function stream_chat() {
   // Get data
-  var stream_input= document.getElementById('stream_input');
+  var stream_input = document.getElementById('stream_input');
   // Open our chat window
   newwindow=window.open('https://www.twitch.tv/'+stream_input.value+'/chat?popout=','name','height=600,width=400');
   // Focus on it
   if (window.focus) {newwindow.focus()}
   return false;
+}
+
+// On load, create new player
+document.onload = function() {
+  // Create our player
+  var options = {
+      width: '100%',
+      height: '100%',
+      channel: gapi.hangout.data.getValue('stream_input'), 
+      //video: "{VIDEO_ID}"
+  };
+  // Insert
+  Twitch.Player = new Twitch.Player("live_player", options); 
 }
